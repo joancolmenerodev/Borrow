ac4c76db540a1aa22dcc3e2c9e42580952230cd9
Joan <joancolmenero@gmail.com>
2020-10-15 20:43:14 +0200

    regrouped features

--- buildSrc/src/main/kotlin/ModuleDependencies.kt
+++ buildSrc/src/main/kotlin/ModuleDependencies.kt
@@ -1,8 +1,8 @@
 object ModuleDependencies {
     const val APP = ":app"
     const val FIREBASE = ":firebase"
-    const val LOGIN = ":login"
+    const val LOGIN = ":features:login"
     const val SHARED = ":shared"
     const val COMMONS_UI = ":commons-ui"
-    const val HOME = ":home"
+    const val HOME = ":features:home"
 }
